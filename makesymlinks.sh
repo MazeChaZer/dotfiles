1556c44d19ba04c13e546530df471d31562128ac
Jonas Schürmann <jonasschuermann@aol.de>
2014-11-08 17:14:43 +0100

    Added .pentadactylrc.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc gvimrc Xresources i3 Xmodmap"    # list of files/folders to symlink in homedir
+files="bashrc vimrc gvimrc Xresources i3 Xmodmap pentadactylrc"    # list of files/folders to symlink in homedir
 
 ##########
 
